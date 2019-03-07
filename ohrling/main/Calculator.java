49cb34c5a6a4664f996531a57e25e007618cf4fa
Ohrling <ohrling@users.noreply.github.com>
2019-03-07 10:52:55 +0100

    Refactor step, green single expression green multiple expression

--- ohrling/main/Calculator.java
+++ ohrling/main/Calculator.java
@@ -3,96 +3,38 @@ package main;
 public class Calculator {
     public String calculateExpression(String expression) {
         Double sum = null;
-        Double d1 = null;
         Double d2 = null;
         char term = '!';
-        String regex = "(?<=[-+*/])|(?=[-+*/])";
+        String regex = "(?<=[-+*/%])|(?=[-+*/%])";
         String[] splitted = expression.split(regex);
         for (int i = 0; i < splitted.length; i++) {
-            if(d1 == null)
-                d1 = convertStringToDouble(splitted[i]);
+            if(sum == null)
+                sum = convertStringToDouble(splitted[i]);
             else if(d2 == null)
                 d2 = convertStringToDouble(splitted[i]);
-            if(d1 == null || d2 == null)
+            if(sum == null || d2 == null)
                 term = splitted[i].charAt(0);
-            if(d1 != null && d2 != null && term != '!') {
+            if(sum != null && d2 != null && term != '!') {
                 switch (term) {
                     case '+':
-                        sum = addition(d1,d2);
+                        sum = addition(sum,d2);
                         break;
                     case '-':
-                        sum = subtraction(d1,d2);
+                        sum = subtraction(sum,d2);
                         break;
                     case '*':
-                        sum = multiplication(d1,d2);
+                        sum = multiplication(sum,d2);
                         break;
                     case '/':
-                        sum = division(d1,d1);
+                        sum = division(sum,d2);
                         break;
                     case '%':
-                        sum = modulus(d1,d2);
+                        sum = modulus(sum,d2);
                         break;
                 }
-                d1 = null;
                 d2 = null;
             }
-        }/*
-        if(expression.contains("+")) {
-            splitted = expression.split("([+ -])");
-            for (String num :
-                    splitted) {
-                if(!num.equalsIgnoreCase("+") || !num.equalsIgnoreCase("-")) {
-                    sum = addition(sum, convertStringToDouble(num));
-                }
-            }
-        } else if(expression.contains("-")) {
-            splitted = expression.split("([-])");
-            for (String num :
-                    splitted) {
-                if(!num.equalsIgnoreCase("-")) {
-                    if (sum == 0.0) {
-                        sum = convertStringToDouble(num);
-                    } else {
-                        sum = subtraction(sum, convertStringToDouble(num));
-                    }
-                }
-            }
-         }else if(expression.contains("*")) {
-            splitted = expression.split("([*])");
-            for(int i = 0; i < splitted.length; i++) {
-                if(!splitted[i].equalsIgnoreCase("*")){
-                    if(i == 0) {
-                        sum = convertStringToDouble(splitted[i]);
-                    } else {
-                        sum = multiplication(sum, convertStringToDouble(splitted[i]));
-                    }
-                }
-            }
-        } else if(expression.contains("/")) {
-            splitted = expression.split("([/])");
-            for(int i = 0; i < splitted.length; i++){
-                if(!splitted[i].equalsIgnoreCase("/")) {
-                    if(i == 0) {
-                        sum = convertStringToDouble(splitted[i]);
-                    } else {
-                        sum = division(sum, convertStringToDouble(splitted[i]));
-                    }
-                }
-            }
-        } else if(expression.contains("%")) {
-            splitted = expression.split("([%])");
-            for(int i = 0; i < splitted.length; i++){
-                if(!splitted[i].equalsIgnoreCase("/")) {
-                    if(i == 0) {
-                        sum = convertStringToDouble(splitted[i]);
-                    } else {
-                        sum = modulus(sum, convertStringToDouble(splitted[i]));
-                    }
-                }
-            }
-        } else {
-            sum = convertStringToDouble(expression);
-        }*/
+        }
         return sum.toString();
     }
 
